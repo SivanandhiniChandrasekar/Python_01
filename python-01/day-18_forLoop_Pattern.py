195d4304f903a5dcb0debdde28b0ffc4a1cb1abb
SivanandhiniChandrasekar <sivanandhinic31@gmail.com>
2026-08-14 17:35:10 +0530

    Patterns

---
+++ python-01/day-18_forLoop_Pattern.py
@@ -0,0 +1,49 @@
+# for(int i = 0; i < n; i++){
+    
+# }
+
+# Initiate
+# Condition
+# Increment
+
+# calculator
+# add()
+# sub()
+# mulitplication()
+# def add():
+#     pass
+# for i in range(11): 
+#     print(i,end = " ")
+
+# for i in range(1, 12, 2):
+#     print(i, end = " ")
+
+for i in range(12, 0, -1):
+    print(i, end = " ")
+# break, continue, pass
+
+# print("Sivs"+"nandhini") 
+# print()
+# 0 default, increment by 1 Default
+
+# s = "aswini"
+# for i in s:
+
+n = 5
+
+# # $ # $ #
+# $ # $ # $ 
+# # $ # $ #
+# $ # $ # $ 
+# # $ # $ #
+
+
+n = 4
+
+# 0
+# 1 1
+# 2 3 5
+# 8 13 21 34
+
+# right triangle
+# fibonacci series
---
+++ python-01/day-19_pattern.py
@@ -0,0 +1,69 @@
+# Inverted Pyramid
+# Diamond Pattern
+
+# ******* - 7
+#  *****  - 5
+#   ***   - 3
+#    *    - 1
+
+# Row count - 4
+# space  - 0, 1, 2, 3
+# star (pattern) - 
+
+# n = 4
+# for i in range(n):
+#     for j in range(n - i):
+#         print(" ", end = "")
+#     for k in range(2 * i -1):
+#         print("*", end = "")
+#     print()
+# for i in range(n, 0, -1):
+#     for j in range(n - i):
+#         print(" ", end = "")
+#     for k in range(2 * i -1):
+#         print("*", end = "")
+#     print()
+
+# Hollow pyramid
+# 1234567
+#1   *
+#2  * *
+#3 *   *
+# #4*******
+# n = 4
+# for i in range(1, n + 1):
+#     for j in range(n - i):
+#         print(" ", end = "")
+#     for k in range(2 * i - 1):
+#         if k == 0 or k == 2 * i - 2 or i == n:
+#             print("*", end = "")
+#         else:
+#             print(" ", end = "")
+#     print()
+    
+n = 5
+# 1       5
+#   2   4
+#     3
+#   2   4
+# 1       5
+
+# Pascal's Triangle
+#     1
+#    1 1
+#   1 2 1
+#  1 3 3 1
+# 1 4 6 4 1
+
+# row count = 5
+# colum changes = space -- decreasing, numbers -- increasing
+
+# column 1 - 1
+# column 2 - 1 1
+# column 3 
+# column 4 
+# column 5
+
+#  num = 1
+#  1 - 2 - 1
+# num, 
